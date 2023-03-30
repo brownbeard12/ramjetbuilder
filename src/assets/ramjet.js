be410b6671f6db294ec22c1ee662e71ae79fd8a5
brownbeard12 <31408027+brownbeard12@users.noreply.github.com>
2023-03-30 10:47:26 -0400

    colors

--- src/assets/ramjet.js
+++ src/assets/ramjet.js
@@ -3,15 +3,20 @@ import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 
 const scene = new THREE.Scene();
 
-const light1 = new THREE.PointLight(0xff0000, 50, 100);
+const light1 = new THREE.PointLight(0xffffff, 20, 100);
 light1.position.set(1, 1, 5);
+light1.add(new THREE.Mesh(new THREE.SphereGeometry(0.1), new THREE.MeshBasicMaterial({ color: 0xffffff })))
 scene.add(light1);
 
-const light2 = new THREE.PointLight(0xff0000, 20, 100);
+const light2 = new THREE.PointLight(0xffffff, 20, 100);
 light2.position.set(3, 3, 8);
+light2.add(new THREE.Mesh(new THREE.SphereGeometry(0.1), new THREE.MeshBasicMaterial({ color: 0xffffff })))
 scene.add(light2);
 
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const ambLight = new THREE.AmbientLight(0x404040);
+scene.add(ambLight);
+
+const camera = new THREE.PerspectiveCamera(35, window.innerWidth / window.innerHeight, 0.1, 1000);
 
 const renderer = new THREE.WebGLRenderer();
 const margin = 50;
@@ -21,7 +26,7 @@ document.body.appendChild(renderer.domElement);
 const controls = new OrbitControls(camera, renderer.domElement);
 
 const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshStandardMaterial({ color: 0x00ff00 });
+const material = new THREE.MeshStandardMaterial({ color: 0xff0000 });
 const cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
 
@@ -32,6 +37,9 @@ function animate() {
     renderer.setSize(window.innerWidth - margin, window.innerHeight - margin);
     camera.aspect = window.innerWidth / window.innerHeight;
     renderer.render(scene, camera);
+
+    light1.position.x -= 0.02
+    light2.position.z -= 0.02
 }
 
 animate();
