59f4a6e1fd85cd3d9875ee69dc49091f128c00e5
brownbeard12 <31408027+brownbeard12@users.noreply.github.com>
2023-03-30 20:12:40 -0400

    dim adjustment control

--- src/assets/ramjet.js
+++ src/assets/ramjet.js
@@ -1,10 +1,19 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import GUI from 'lil-gui';
+
+const gui = new GUI();
+
+const obj = {
+    xDim: 1.0,
+    yDim: 1.0,
+    zDim: 1.0,
+};
 
 const scene = new THREE.Scene();
 
 const light1 = new THREE.PointLight(0xffffff, 20, 100);
-light1.position.set(1, 1, 5);
+light1.position.set(-3, -1, 5);
 light1.add(new THREE.Mesh(new THREE.SphereGeometry(0.1), new THREE.MeshBasicMaterial({ color: 0xffffff })))
 scene.add(light1);
 
@@ -27,19 +36,26 @@ const controls = new OrbitControls(camera, renderer.domElement);
 
 const geometry = new THREE.BoxGeometry(1, 1, 1);
 const material = new THREE.MeshStandardMaterial({ color: 0xff0000 });
-const cube = new THREE.Mesh(geometry, material);
+let cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
 
 camera.position.z = 5;
 
+gui.add(obj, 'xDim', 0.5, 3.0).onChange( val => { obj.xDim = val; regen(cube, obj) });
+gui.add(obj, 'yDim', 0.5, 3.0).onChange( val => { obj.yDim = val; regen(cube, obj) });
+gui.add(obj, 'zDim', 0.5, 3.0).onChange( val => { obj.zDim = val; regen(cube, obj) });
+
 function animate() {
     requestAnimationFrame(animate);
     renderer.setSize(window.innerWidth - margin, window.innerHeight - margin);
-    camera.aspect = window.innerWidth / window.innerHeight;
     renderer.render(scene, camera);
 
-    light1.position.x -= 0.01
-    light2.position.z -= 0.01
+}
+
+function regen(mesh, val) {
+    scene.remove(mesh);
+    cube = new THREE.Mesh(new THREE.BoxGeometry(obj.xDim, obj.yDim, obj.zDim), material);
+    scene.add(cube);
 }
 
 animate();
