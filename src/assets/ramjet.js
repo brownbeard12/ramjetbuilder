26c40ff0e482a4d85a9a5c487160ddff8ff0a9e2
brownbeard12 <31408027+brownbeard12@users.noreply.github.com>
2023-06-29 15:48:14 -0400

    hdr + matl

--- src/assets/ramjet.js
+++ src/assets/ramjet.js
@@ -1,7 +1,7 @@
 import * as THREE from "three";
 import { OrbitControls } from "three/addons/controls/OrbitControls.js";
+import { RGBELoader } from "three/addons/loaders/RGBELoader.js";
 import GUI from "lil-gui";
-//import { HemisphereLight } from 'three';
 
 const gui = new GUI();
 
@@ -23,7 +23,21 @@ coneGUI.diffAngle =
 
 const scene = new THREE.Scene();
 
-const sky = new THREE.HemisphereLight(0xffffff, 0xffffff, 2);
+const loader = new RGBELoader();
+
+loader
+  .setPath("src/assets/")
+  .load("kloppenheim_06_puresky_4k.hdr", function (texture) {
+    texture.mapping = THREE.EquirectangularReflectionMapping;
+    scene.background = texture;
+    scene.environment = texture;
+  });
+
+const envText = loader
+  .setPath("src/assets/")
+  .load("kloppenheim_06_puresky_4k.hdr");
+
+const sky = new THREE.HemisphereLight(0xffffff, 0xffffff, 20);
 scene.add(sky);
 
 const light1 = new THREE.PointLight(0xffffff, 20, 1000);
@@ -34,7 +48,7 @@ light1.add(
     new THREE.MeshBasicMaterial({ color: 0xffffff })
   )
 );
-scene.add(light1);
+//scene.add(light1);
 
 const light2 = new THREE.PointLight(0xffffff, 20, 1000);
 light2.position.set(3, 0, 8);
@@ -44,7 +58,7 @@ light2.add(
     new THREE.MeshBasicMaterial({ color: 0xffffff })
   )
 );
-scene.add(light2);
+//scene.add(light2);
 
 const camera = new THREE.PerspectiveCamera(
   35,
@@ -58,13 +72,14 @@ const margin = 50;
 renderer.setSize(window.innerWidth - margin, window.innerHeight - margin);
 document.body.appendChild(renderer.domElement);
 
-const ambLight = new THREE.HemisphereLight(0x000000, 0xffffff, 1);
-scene.add(ambLight);
-
 const controls = new OrbitControls(camera, renderer.domElement);
 
 const coneGeo = new THREE.ConeGeometry(2, 4, 128, 16);
-const coneMat = new THREE.MeshStandardMaterial({ color: 0xff0000 });
+const coneMat = new THREE.MeshPhysicalMaterial({
+  color: 0xbbbcc4,
+  roughness: 0.3,
+  metalness: 1.0,
+});
 let diffuser = new THREE.Mesh(coneGeo, coneMat);
 diffuser.rotation.x = Math.PI / 2;
 diffuser.position.z = coneGUI.diffPos;
@@ -79,15 +94,12 @@ const bodyCross = [
   new THREE.Vector2(bodyOuterDiam / 2 - bodyThickness, 0),
   new THREE.Vector2(bodyOuterDiam / 2, 0),
 ];
-const jetBodyGeo = new THREE.LatheGeometry(
-    bodyCross,
-    64
-);
+const jetBodyGeo = new THREE.LatheGeometry(bodyCross, 128);
 const jetBody = new THREE.Mesh(jetBodyGeo, coneMat);
 jetBody.rotation.x = -Math.PI / 2;
 scene.add(jetBody);
 
-camera.position.z = 5;
+camera.position.z = 50;
 
 gui.add(coneGUI, "coneRAD", 1.0, 3.0, 0.1).onChange((val) => {
   coneGUI.coneRAD = val;
@@ -103,20 +115,14 @@ gui.add(coneGUI, "diffPos", -2, 5, 0.1).onChange((val) => {
   regen(diffuser, coneGUI);
 });
 
+renderer.render(scene, camera);
+
 let i = 0;
 
 function animate() {
   requestAnimationFrame(animate);
   renderer.setSize(window.innerWidth - margin, window.innerHeight - margin);
   renderer.render(scene, camera);
-
-  light1.position.x = 3 * Math.sin(i * Math.PI);
-  light1.position.y = 3 * Math.cos(i * Math.PI);
-
-  light2.position.x = 4 * Math.sin(i * Math.PI);
-  light2.position.z = 4 * Math.cos(i * Math.PI);
-
-  i += 0.001;
 }
 
 function regen(mesh, val) {
